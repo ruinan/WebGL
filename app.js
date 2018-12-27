aa7c8c8489d92c848df12e14d2c255b69b537dc6
Rui Nan <rui@stronghold.co>
2018-12-27 15:51:38 -0800

    add babel, webpack

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 class App extends React.Component {
     render() {
         return (
-            <p>hellow</p>
+            <p>ccc</p>
         );
     }
 } 
 
-ReactDOM.render(App, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
