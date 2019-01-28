68a8284a79b74ac769f3f7b20f39f0483241d179
Rui Nan <rui@stronghold.co>
2019-01-28 11:50:23 -0800

    completed Lasso tool

--- lassoTool/server.js
+++ lassoTool/server.js
@@ -1,33 +1,26 @@
-var express = require('express');
-var fs = require('fs');
+const express = require('express');
+const fs = require('fs');
 const fileUpload = require('express-fileupload');
-var app = express();
-// const fileUpload = require('express-fileupload');
-// var multer  = require('multer');
-// var storage = multer.diskStorage({
-//     destination: function (req, file, cb) {
-//       cb(null, 'public/images/')
-//     },
-//     filename: function (req, file, cb) {
-//         console.log();
-//       cb(null, file.fieldname)
-//     }
-//   })
-// var upload = multer({ storage })
+const app = express();
 
 app.use(express.static('public'));
 app.use(fileUpload());
+
+// return the html page
 app.get('/', (req, res) => {
     res.sendFile('./public/index.html');
     res.end();
 });
 
+// upload the picture
 app.post('/image', (req, res) => {
+
     if (Object.keys(req.files).length == 0) {
         return res.status(400).send('No files were uploaded.');
     }
 
-    // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+    // use the same image name that will automatically load the new picture when page redirection happens
+    // delete the previous one
     let sampleFile = req.files.picture;
     fs.unlink('./public/images/test.jpg', err => {
         if (err) {
@@ -35,7 +28,7 @@ app.post('/image', (req, res) => {
         }
         console.log('text.jpg deleted');
     });
-    // Use the mv() method to place the file somewhere on your server
+    // set the new one
     sampleFile.mv(`./public/images/test.jpg`, function(err) {
         if (err) return res.status(500).send(err);
         res.redirect('/');
@@ -43,5 +36,5 @@ app.post('/image', (req, res) => {
 });
 
 app.listen(3000, function() {
-    console.log('Example app listening on port 3000!');
+    console.log('Example app listening on port 3000');
 });
